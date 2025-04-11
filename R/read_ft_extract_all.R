aab4c7fd673e48723ac174a3bc5de5f11bb366e3
chaochaowong <chao-jen.wong@seattlechildrens.org>
2025-04-11 15:31:43 -0700

    fixed bugs in read_ft_extract_all()

--- R/read_ft_extract_all.R
+++ R/read_ft_extract_all.R
@@ -1,39 +1,43 @@
-#` @export
-read_ft_extract_all <- function(file, col_names = NULL, genome_info = NULL, overlap_ranges = NULL) {
+# ` @export
+read_ft_extract_all <- function(file, col_names = NULL,
+                                genome_info = NULL,
+                                overlap_ranges = NULL) {
   args <- plyranges:::norm_args_reader(genome_info)
-  extra_cols <- col_names <- c(sam_flag = 'character', 
-                               HP = 'character',
-                               RG = 'character',
-                               fiber_length = 'numeric', 
-                               fiber_sequence = 'DNAStringSet',  
-                               ec = 'character',
-                               rq = 'character',
-                               total_AT_bp = 'numeric',
-                               total_m6a_bp = 'numeric',
-                               total_nuc_bp = 'numeric',
-                               total_msp_bp = 'numeric',
-                               total_5mC_bp = 'numeric',
-                               nuc_starts = 'character',
-                               nuc_lengths = 'character',
-                               ref_nuc_starts = 'character',
-                               ref_nuc_lengths = 'character', 
-                               msp_starts = 'character', 
-                               msp_lengths = 'character',
-                               fire = 'character',
-                               ref_msp_starts = 'character',
-                               ref_msp_lengths = 'character', 
-                               m6a = 'character', 
-                               ref_m6a = 'character', 
-                               m6a_qual = 'character', 
-                               `5mC` = 'character',
-                               ref_5mC = 'character', 
-                               `5mC_qual` = 'character')
-  
-  rtracklayer::import.bed(file, 
-                          colnames = col_names, 
-                          extraCols = extra_cols,
-                          genome = args$genome_info,
-                          seqinfo = args$seq_info,
-                          which = overlap_ranges)
-  
+  extra_cols <- c(
+    sam_flag = "character",
+    HP = "character",
+    RG = "character",
+    fiber_length = "numeric",
+    fiber_sequence = "DNAStringSet",
+    ec = "character",
+    rq = "character",
+    total_AT_bp = "numeric",
+    total_m6a_bp = "numeric",
+    total_nuc_bp = "numeric",
+    total_msp_bp = "numeric",
+    total_5mC_bp = "numeric",
+    nuc_starts = "character",
+    nuc_lengths = "character",
+    ref_nuc_starts = "character",
+    ref_nuc_lengths = "character",
+    msp_starts = "character",
+    msp_lengths = "character",
+    fire = "character",
+    ref_msp_starts = "character",
+    ref_msp_lengths = "character",
+    m6a = "character",
+    ref_m6a = "character",
+    m6a_qual = "character",
+    `5mC` = "character",
+    ref_5mC = "character",
+    `5mC_qual` = "character"
+  )
+
+  rtracklayer::import.bed(file,
+    colnames = col_names,
+    extraCols = extra_cols,
+    genome = args$genome_info,
+    seqinfo = args$seq_info,
+    which = overlap_ranges
+  )
 }
